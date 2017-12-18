82c652f55777e3d07c676145215bf17ed4ef6e91
simon <simon10030950@gmail.com>
2017-12-18 21:33:48 +0800

    move apriltags_detection to catkin_ws

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/car/SIS-Autonomous_Cars/apriltags_detection/catkin_ws/src
+# Build directory: /home/car/SIS-Autonomous_Cars/apriltags_detection/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(apriltags_ros/apriltags)
+subdirs(adafruit_drivers)
+subdirs(me212bot)
+subdirs(raspicam_node)
+subdirs(apriltags_ros/apriltags_ros)
---
+++ catkin_ws/build/raspicam_node/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/car/SIS-Autonomous_Cars/apriltags_detection/catkin_ws/src/raspicam_node
+# Build directory: /home/car/SIS-Autonomous_Cars/apriltags_detection/catkin_ws/build/raspicam_node
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
