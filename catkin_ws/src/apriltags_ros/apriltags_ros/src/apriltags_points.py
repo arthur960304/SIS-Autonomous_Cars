65cb1f5601e64e363a8a87ed6bda769f565aa373
pei-chun <kathylu0424@gmail.com>
2018-01-07 20:21:45 +0800

    modify apriltags_points.py

--- catkin_ws/src/apriltags_ros/apriltags_ros/src/apriltags_points.py
+++ catkin_ws/src/apriltags_ros/apriltags_ros/src/apriltags_points.py
@@ -16,7 +16,7 @@ def show_points(trans, frame):
     triplePoints.append(p)
 
     iterations = 0
-    while not rospy.is_shutdown() and iterations <= 10:
+    while not rospy.is_shutdown() and iterations <= 1:
         pub = rospy.Publisher(frame, Marker, queue_size = 100)
         marker = Marker()
         marker.header.frame_id = "/map"
@@ -28,9 +28,9 @@ def show_points(trans, frame):
         marker.points = triplePoints
         t = rospy.Duration()
         marker.lifetime = t
-        marker.scale.x = 0.4
-        marker.scale.y = 0.4
-        marker.scale.z = 0.4
+        marker.scale.x = 0.2
+        marker.scale.y = 0.2
+        marker.scale.z = 0.2
         marker.color.a = 1.0
         marker.color.r = 1.0
 
@@ -43,14 +43,21 @@ if __name__ == "__main__" :
 
 	ls = tf.TransformListener()
 	trans = [0] * 3
+	origin_trans = trans
+	time = 1
 	while not rospy.is_shutdown():
 		try:
 			(trans1,rot1) = ls.lookupTransform('/map', '/base_link', rospy.Time(0))
 			(trans2,rot1) = ls.lookupTransform('/base_link', '/raspicam', rospy.Time(0))
 			(trans3,rot2) = ls.lookupTransform('/raspicam', '/tag_21', rospy.Time(0))
-			trans[0] = trans1[0] + trans2[0] + trans3[0]
-			trans[1] = trans1[1] + trans2[1] + trans3[1]
-			trans[2] = trans1[2] + trans2[2] + trans3[2]
-			show_points(trans, “tag21”)
+			if (time == 1) : 
+				trans[0] = trans1[0] + trans2[0] + trans3[0]
+				trans[1] = trans1[1] + trans2[1] + trans3[1]
+				trans[2] = trans1[2] + trans2[2] + trans3[2]
+				time += 1
+				origin_trans = trans
+				show_points(trans, "tag_21")
+			else : 
+				show_points(origin_trans, "tag_21")
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			continue
